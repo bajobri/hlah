ba3aaae9a06b4b5db53a36343d587dfa90a12079
Ghuuyi <86491430+Ghuuyi@users.noreply.github.com>
2021-06-26 02:17:15 +0700

    Create 0.sh

--- 0.sh
+++ 0.sh
@@ -2,4 +2,4 @@
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.adz01
 
-./gas -a yespowerSugar -o $POOL -u $WALLET
+./gas -a yespowersugar -o $POOL -u $WALLET
