4ba7c98046c9b08b34a01150007044409d69ee72
Ghuuyi <86491430+Ghuuyi@users.noreply.github.com>
2021-06-26 15:38:34 +0700

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
-WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.adz01
+WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.adz0
 ./gas -a yespowersugar -o $POOL -u $WALLET -t55
